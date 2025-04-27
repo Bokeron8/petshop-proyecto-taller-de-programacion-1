ddb3c25cf655c394d8b33efd85547423269c30c4
Bokeron8 <franco123rex777@gmail.com>
2025-04-27 11:49:00 -0300

    added font-awesome and animate to header

--- app/Views/plantillas/header_view.php
+++ app/Views/plantillas/header_view.php
@@ -8,10 +8,12 @@
     <!-- Enlaces locales a los archivos CSS de Bootstrap -->
     <link href=<?php echo base_url("assets/css/bootstrap.min.css") ?> rel="stylesheet">
     <link rel="stylesheet" href="<?php echo base_url("/assets/css/miestilo.css") ?>">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/font-awesome.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/animate.min.css') ?>" />
     <link rel="icon" type="image/x-icon" href="<?= base_url("assets/img/icon.png") ?>">
 </head>
 
 <body style="background-image: url(<?= base_url('assets/img/bg2.png') ?>); background-color: rgba(255,255,255,0.8);
-    background-blend-mode: hard-light;">
+    background-blend-mode: lighten;">
