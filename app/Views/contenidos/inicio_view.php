49bc96fb401909f1251bc52011b6e2a402de6099
Bokeron8 <franco123rex777@gmail.com>
2025-04-28 20:30:13 -0300

    add another card

--- app/Views/contenidos/inicio_view.php
+++ app/Views/contenidos/inicio_view.php
@@ -7,14 +7,15 @@
 $cards = [
     ["titulo" => "Alimentos", "texto" => "Descripción breve de la tarjeta 1.", "href" => '#', "alt" => "Imagen de comida para perros", "imgSrc" => "assets/img/card_alimento.webp"],
     ["titulo" => "Accesorios y Juguetes", "texto" => "Descripción breve de la tarjeta 1.", "href" => '#', "alt" => "Imagen de juguetes para perros", "imgSrc" => "assets/img/card_juguetes.jpg"],
-    ["titulo" => "Higiene y medicamentos", "texto" => "Descripción breve de la tarjeta 1.", "href" => '#', "alt" => "Imagen de shampoos para perros y gatos", "imgSrc" => "assets/img/card_shampoo.webp"]
+    ["titulo" => "Higiene", "texto" => "Descripción breve de la tarjeta 1.", "href" => '#', "alt" => "Imagen de shampoos para perros y gatos", "imgSrc" => "assets/img/card_shampoo.webp"],
+    ["titulo" => "Medicamentos", "texto" => "Consulta médica, vacunación y más.", "href" => '#', "alt" => "Imagen de veterinario atendiendo a un perro", "imgSrc" => "assets/img/card_veterinario.jpg"]
 ];
 ?>
 
 
-<section class="container mt-4">
+<section class="container mt-4 mb-4">
     <!-- Componente del carrusel -->
-    <img src="<?= base_url('assets/img/petshop_hero.jpg') ?>" class="d-block w-100 carousel-hero rounded"
+    <img src="<?= base_url('assets/img/petshop_hero.png') ?>" class="d-block w-100 carousel-hero rounded"
         alt="Portada petshop">
 </section>
 <!-- Fin del carrusel -->
@@ -26,9 +27,9 @@ $cards = [
         <!-- Primer tarjeta: Ocupa 4 columnas en pantallas medianas -->
         <?php foreach ($cards as $titulo => $value): ?>
 
-            <div class="col-md-4 mb-2">
+            <div class="col-md-3 col-6 mb-2">
                 <!-- Componente de tarjeta de Bootstrap -->
-                <div class="card">
+                <div class="card h-100">
                     <!-- Imagen de la tarjeta -->
                     <img src="<?= base_url($value['imgSrc']) ?>" class="card-img-top carta-productos"
                         alt="<?= $value['alt'] ?>">
