45d5803ee03d73b9d9a337a76ebac61e9d8d20d2
Bokeron8 <franco123rex777@gmail.com>
2025-04-29 01:33:52 -0300

    fuentes de texto optimizadas para conexiones mas lentas

--- app/Views/plantillas/header_view.php
+++ app/Views/plantillas/header_view.php
@@ -13,6 +13,11 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/font-awesome.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/animate.min.css') ?>" />
     <link rel="icon" type="image/x-icon" href="<?= base_url("assets/img/icon.png") ?>">
+
+    <link rel="preload" href="<?= base_url('assets/fonts/catpaw.otf') ?>" as="font" type="font/otf" crossorigin="anonymous">
+    <link rel="preload" href="<?= base_url('assets/fonts/puppy.ttf') ?>" as="font" type="font/ttf" crossorigin="anonymous">
+    <link rel="preload" href="<?= base_url('assets/fonts/honey.woff2') ?>" as="font" type="font/woff2" crossorigin="anonymous">
+
 </head>
 
 <body>
