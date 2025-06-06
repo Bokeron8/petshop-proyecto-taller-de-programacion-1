aafcca6047be2a6ced2ad0451a31c042df475bee
Bokeron8 <franco123rex777@gmail.com>
2025-06-06 09:58:10 -0300

    catagoria_producto_model agregado

---
+++ app/Models/Categoria_Producto_Model.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Categoria_Producto_Model extends Model
+{
+  protected $table = 'categorias_productos'; // nombre de tu tabla
+  protected $primaryKey = 'id_categorias_producto';
+
+  protected $useAutoIncrement = true;
+
+  protected $returnType     = 'array';
+
+  protected $allowedFields = ['id_producto', 'id_categoria'];
+}
