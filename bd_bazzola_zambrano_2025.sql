acce76d3c798289242f6ccb2c5a55e79f88117e6
Bokeron8 <franco123rex777@gmail.com>
2025-06-17 12:04:33 -0300

    producto y compras agregadas

--- bd_bazzola_zambrano_2025.sql
+++ bd_bazzola_zambrano_2025.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-06-2025 a las 17:03:28
+-- Tiempo de generación: 17-06-2025 a las 17:03:43
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -60,13 +60,14 @@ CREATE TABLE `categorias_productos` (
 --
 
 INSERT INTO `categorias_productos` (`id_categorias_producto`, `id_producto_categorias_productos`, `id_categoria_categorias_productos`) VALUES
-(32, 16, 1),
-(33, 17, 3),
-(34, 18, 3),
-(35, 19, 3),
-(36, 20, 1),
-(37, 21, 1),
-(38, 22, 1);
+(42, 18, 3),
+(43, 19, 3),
+(44, 20, 1),
+(45, 21, 1),
+(46, 22, 1),
+(47, 16, 1),
+(48, 17, 3),
+(49, 23, 1);
 
 -- --------------------------------------------------------
 
@@ -81,6 +82,28 @@ CREATE TABLE `detalle_venta` (
   `detalle_precio` decimal(10,0) NOT NULL,
   `detalle_subtotal` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `detalle_venta`
+--
+
+INSERT INTO `detalle_venta` (`id_venta`, `id_producto`, `detalle_cantidad`, `detalle_precio`, `detalle_subtotal`) VALUES
+(1, 16, 1, 22000, 22000),
+(1, 18, 1, 13, 13),
+(1, 19, 2, 13, 25),
+(2, 17, 2, 13, 25),
+(3, 16, 2, 22000, 44000),
+(4, 20, 1, 6000, 6000),
+(4, 21, 1, 3000, 3000),
+(4, 22, 1, 41000, 41000),
+(5, 20, 1, 6000, 6000),
+(6, 16, 1, 22000, 22000),
+(7, 16, 1, 22000, 22000),
+(8, 16, 1, 22000, 22000),
+(9, 17, 1, 13, 13),
+(10, 16, 1, 22000, 22000),
+(11, 16, 1, 22000, 22000),
+(12, 17, 2, 13, 25);
 
 -- --------------------------------------------------------
 
@@ -124,7 +147,7 @@ CREATE TABLE `mensajes` (
 --
 
 INSERT INTO `mensajes` (`id_mensaje`, `nombre_mensaje`, `email_mensaje`, `mensaje`, `fecha_mensaje`) VALUES
-(10, 'mensajes', 'nospamfranco3@gmail.com', 'qqqqqqqqqqqqqqqqqqqqqqq', '2025-06-12');
+(10, 'juan perez', 'juanperez@gmail.com', 'mensaje de prueba ', '2025-06-12');
 
 -- --------------------------------------------------------
 
@@ -167,13 +190,14 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id_producto`, `nombre_producto`, `descripcion_producto`, `id_marca_producto`, `stock_producto`, `precio_producto`, `imagen_producto`, `estado_producto`) VALUES
-(16, 'Bolsa de Alimento 20kg', 'Bolsa de Alimento 20kg Dr Perrot. Perro Adulto.', 3, 20, 22000, '1749735374_5925eebc723e54458017.png', 1),
-(17, 'Bola Leon', 'Bola Leon para jugar y masticar', 6, 5, 12.5, '1749236644_b33cb4c09d834a956a51.jpg', 1),
-(18, 'Bola Tigre', 'Bola Tigre ', 6, 6, 12.5, '1749236697_54665aa688d4810ffb6a.jpg', 1),
-(19, 'Bola de Mono', 'Bola de Mono', 6, 7, 12.5, '1749236775_6072896120044aa2d056.jpg', 1),
-(20, 'Bolsa de Alimento 3kg', 'Bolsa de Alimento 3kg. Pedigree. Perro Adulto.', 1, 25, 6000, '1749736776_2270f3139c8809163d85.avif', 1),
-(21, 'Bolsa de Alimento 1.5kg', 'Bolsa de Alimento 1.5kg. Pedigree. Perro Adulto.', 1, 20, 3000, '1749737107_b281301906f97ffff350.avif', 1),
-(22, 'Bolsa de Alimento 21kg', 'Bolsa de Alimento 21kg. Pedigree. Perro Adulto.', 1, 20, 41000, '1749737175_e24b0d5b858778572bb5.avif', 1);
+(16, 'Bolsa de Dr.Perrot 20kg', 'Bolsa de Alimento 20kg Dr Perrot. Perro Adulto.', 3, 12, 22000, '1749735374_5925eebc723e54458017.png', 1),
+(17, 'Pelota Leon', 'Pelota Leon para que tu mascota juegue.', 6, 23, 12.5, '1749236644_b33cb4c09d834a956a51.jpg', 1),
+(18, 'Pelota Tigre', 'Pelota Tigre ', 6, 5, 12.5, '1749236697_54665aa688d4810ffb6a.jpg', 1),
+(19, 'Pelota Mono', 'Pelota Mono', 6, 5, 12.5, '1749236775_6072896120044aa2d056.jpg', 1),
+(20, 'Bolsa de Pedigree 3kg', 'Bolsa de Alimento 3kg. Pedigree. Perro Adulto.', 1, 23, 6000, '1749736776_2270f3139c8809163d85.avif', 1),
+(21, 'Bolsa de Pedigree 1.5kg', 'Bolsa de Alimento 1.5kg. Pedigree. Perro Adulto.', 1, 19, 3000, '1749737107_b281301906f97ffff350.avif', 1),
+(22, 'Bolsa de Pedigree 21kg', 'Bolsa de Alimento 21kg. Pedigree. Perro Adulto.', 1, 19, 41000, '1749737175_e24b0d5b858778572bb5.avif', 1),
+(23, 'Bolsa de Agility Adultos 3kg', 'Bolsa de Agility Adultos 3kg. Talla mediana.', 5, 23, 5000, '1750170821_538dff0b2798c811eb73.webp', 1);
 
 -- --------------------------------------------------------
 
@@ -203,7 +227,9 @@ INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `apellido_usuario`, `dni
 (4, 'mensajes', 'asdasd', 0, '', 'nospamfranco2@gmail.com', '$2y$10$n/cUYTtE2smWaF4fnpgx3.J7ufN2PO86hJSKGRnvRH1KnYMdkv3bu', 1, 0, 1),
 (5, 'mensajes', 'asdasd', 0, '', 'nospamfranco3@gmail.com', '$2y$10$GZWr12fHoPZFLrThyKrT5.Q3FLG0VWtMmMOWm0ZLKvQTOE.mO0th2', 1, 0, 2),
 (6, 'fulanito', 'de tal', 0, '', 'fula@yopmail.com.ar', '$2y$10$kVV4CTMmS8JadbKdVKx0Ge/xX9oMZsm4j72i9bzGxfT19Ev2.lyta', 0, 0, 1),
-(7, 'Menganito', 'de tal', 0, '', 'menga@yopmail.com.ar', '$2y$10$kgJ0fo7gHnde/SrC3ADPaenp1K0og26.Yp.TcrO7MhPfjWGY3261G', 1, 0, 2);
+(7, 'Menganito', 'de tal', 0, '', 'menga@yopmail.com.ar', '$2y$10$kgJ0fo7gHnde/SrC3ADPaenp1K0og26.Yp.TcrO7MhPfjWGY3261G', 1, 0, 2),
+(8, 'full animal', 'administrador', 0, '', 'admin@tienda.com', '$2y$10$jFS2onakGo4X.lufNAKf/uhx5z09B1LvCSiSpHNw9FR0YQBvyrB96', 1, 0, 2),
+(9, 'Marcos', 'Mazzanti', 10000001, 'irigoyen 369 entre don bosco y pago largo', 'marcos@gmail.com', '$2y$10$XloL2Fb1aZV2mDJ6AJMRxORg0KA8rfl.bHhMQRL2dIJuuO4PgUOx.', 1, 2147483647, 1);
 
 -- --------------------------------------------------------
 
@@ -219,6 +245,24 @@ CREATE TABLE `venta` (
   `venta_forma_pago` text NOT NULL,
   `venta_forma_entrega` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `venta`
+--
+
+INSERT INTO `venta` (`venta_id`, `id_cliente`, `venta_fecha`, `venta_total`, `venta_forma_pago`, `venta_forma_entrega`) VALUES
+(1, 9, '2025-06-17', 22038, 'efectivo', 1),
+(2, 9, '2025-06-17', 25, 'transferencia', 0),
+(3, 9, '2025-06-17', 44000, 'efectivo', 1),
+(4, 9, '2025-06-17', 50000, 'efectivo', 0),
+(5, 9, '2025-06-17', 6000, 'efectivo', 0),
+(6, 9, '2025-06-17', 22000, 'efectivo', 0),
+(7, 9, '2025-06-17', 22000, 'efectivo', 0),
+(8, 9, '2025-06-17', 22000, 'efectivo', 0),
+(9, 9, '2025-06-17', 13, 'efectivo', 0),
+(10, 9, '2025-06-17', 22000, 'efectivo', 0),
+(11, 9, '2025-06-17', 22000, 'efectivo', 0),
+(12, 9, '2025-06-17', 25, 'efectivo', 0);
 
 --
 -- Índices para tablas volcadas
@@ -299,13 +343,13 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `categorias_productos`
 --
 ALTER TABLE `categorias_productos`
-  MODIFY `id_categorias_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id_categorias_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT de la tabla `marcas`
 --
 ALTER TABLE `marcas`
-  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT de la tabla `mensajes`
@@ -323,19 +367,19 @@ ALTER TABLE `perfiles`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `venta`
 --
 ALTER TABLE `venta`
-  MODIFY `venta_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `venta_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restricciones para tablas volcadas
