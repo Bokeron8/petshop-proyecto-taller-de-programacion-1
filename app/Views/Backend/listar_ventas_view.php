b3651a7f4bf258df7ab7944795f4269b6c4234a1
Bokeron8 <franco123rex777@gmail.com>
2025-06-18 12:58:08 -0300

    mostrar error al acceder una venta no existente o de otro usuario

--- app/Views/Backend/listar_ventas_view.php
+++ app/Views/Backend/listar_ventas_view.php
@@ -8,6 +8,9 @@
     <?php if (session()->getFlashdata('success')): ?>
         <div class="alert alert-success"><?= session()->getFlashdata('success') ?></div>
     <?php endif; ?>
+    <?php if (session()->getFlashdata('error')): ?>
+        <div class="alert alert-danger"><?= session()->getFlashdata('error') ?></div>
+    <?php endif; ?>
 
     <?php if (empty($ventas)): ?>
         <div class="alert alert-warning">No hay ventas registradas.</div>
